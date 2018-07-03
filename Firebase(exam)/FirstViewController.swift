15ecd6b978a801223868a93be6fd52dc0367105b
Yusuf DEMİRKOPARAN <ysuf.dmrkprn@yusuf-macbook-pro.local>
2018-07-04 00:15:34 +0300

    Initial Commit

---
+++ Firebase(exam)/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Firebase(exam)
+//
+//  Created by Yusuf DEMİRKOPARAN on 4.07.2018.
+//  Copyright © 2018 Yusuf DEMİRKOPARAN. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Firebase(exam)/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Firebase(exam)
+//
+//  Created by Yusuf DEMİRKOPARAN on 4.07.2018.
+//  Copyright © 2018 Yusuf DEMİRKOPARAN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
